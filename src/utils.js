6d20b82ac72f1119a274b6bc7821b41ae18e82f4
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2021-02-15 19:15:08 +0200

    replacing the http request from fetch to xmlhttprequest

--- src/utils.js
+++ src/utils.js
@@ -2,36 +2,40 @@ const DB_NAME = "my-todo";
 
 // Function that returning the data from the server
 function getJsonBinData() {
-  fetch("https://api.jsonbin.io/v3/b/6015baed13b20d48e8bf32fa/latest")
-    .then((response) => response.json())
-    .then((data) => {
-      savedToDoList = data.record["my-todo"];
-      if (savedToDoList === null) return;
-      addingTasksWhenContentLoaded(savedToDoList);
-      hideLoading();
-    })
-    .catch(() => {
+  let xhr = new XMLHttpRequest();
+  xhr.open(
+    "GET",
+    "https://api.jsonbin.io/v3/b/6015baed13b20d48e8bf32fa/latest"
+  );
+  xhr.responseType = "json";
+  xhr.send();
+  xhr.onload = function () {
+    if (xhr.status !== 200) {
       const toDoListUL = document.getElementById("to-do-list");
       const listItem = document.createElement("li");
       toDoListUL.appendChild(listItem);
       listItem.innerHTML = "There is a problem in our servers, hang tight";
       hideLoading();
-    });
+      return;
+    }
+    savedToDoList = xhr.response.record["my-todo"];
+    if (savedToDoList === null) return;
+    addingTasksWhenContentLoaded(savedToDoList);
+    hideLoading();
+  };
 }
 
 // Function that updating the JSONBIN.io.
 function updateJsonBin(toDoListArr) {
   displayLoading();
-  fetch("https://api.jsonbin.io/v3/b/6015baed13b20d48e8bf32fa", {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ "my-todo": toDoListArr }),
-  }).then((res) => {
-    if (!res.ok) {
+  let xhr = new XMLHttpRequest();
+  xhr.open("PUT", "https://api.jsonbin.io/v3/b/6015baed13b20d48e8bf32fa");
+  xhr.setRequestHeader("Content-Type", "application/json");
+  body = JSON.stringify({ "my-todo": toDoListArr });
+  xhr.send(body);
+  xhr.onload = () => {
+    if (xhr.status !== 200)
       alert("There is a problem in our servers, your changes didn't save");
-      hideLoading();
-    } else hideLoading();
-  });
+    hideLoading();
+  };
 }
