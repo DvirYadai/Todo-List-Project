d662dd48cc6d9e7256c2566a77f97d8170fc274a
DvirYadai <74467515+DvirYadai@users.noreply.github.com>
2021-02-18 18:11:50 +0200

    Upadting the utils.js
    changing the utils.js to work with the backend server

--- src/utils.js
+++ src/utils.js
@@ -1,39 +1,53 @@
 // Function that returning the data from the server
 function getJsonBinData() {
-  fetch("https://api.jsonbin.io/v3/b/6015baed13b20d48e8bf32fa/latest")
-    .then((response) => response.json())
-    .then((data) => {
-      savedToDoList = data.record["my-todo"];
-      if (savedToDoList === null) return;
-      addingTasksWhenContentLoaded(savedToDoList);
+  fetch("http://localhost:3000/b/tasks").then(
+    (response) =>
+      response
+        .json()
+        .then((data) => {
+          savedToDoList = data["my-todo"];
+          if (savedToDoList === null) return;
+          addingTasksWhenContentLoaded(savedToDoList);
+          hideLoading();
+        })
+        .catch(() => {
+          const toDoListUL = document.getElementById("to-do-list");
+          const listItem = document.createElement("li");
+          toDoListUL.appendChild(listItem);
+          if (response.status === 400) {
+            listItem.innerHTML = `${response.statusText}, Please enter valid Bin Id.`;
+          } else listItem.innerHTML = `${response.statusText}, Hang tight.`;
+          hideLoading();
+        }),
+    () => {
       hideLoading();
-    })
-    .catch(() => {
       const toDoListUL = document.getElementById("to-do-list");
       const listItem = document.createElement("li");
       toDoListUL.appendChild(listItem);
-      listItem.innerHTML = `There is a problem in our servers, hang tight.</br> Please refresh the page.`;
-      hideLoading();
-    });
+      listItem.innerHTML = `The server is down at this moment, Hang tight.`;
+    }
+  );
 }
 
 // Function that updating the JSONBIN.io.
 function updateJsonBin(toDoListArr) {
   displayLoading();
-  fetch("https://api.jsonbin.io/v3/b/6015baed13b20d48e8bf32fa", {
+  fetch("http://localhost:3000/b/tasks", {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ "my-todo": toDoListArr }),
-  }).then((res) => {
-    if (!res.ok) {
-      console.log(res);
+  })
+    .then((res) => res.json())
+    .then((data) => {
       hideLoading();
+    })
+    .catch(() => {
       const errorDiv = document.createElement("div");
-      errorDiv.innerHTML = `There is a problem in our servers, your changes didn't save. (Error code ${res.status})</br>Please refresh the page.`;
+      errorDiv.innerHTML = `There is a problem in our servers, your changes didn't save.</br> Please refresh the page.`;
       errorDiv.setAttribute("class", "error");
       document.querySelector("body").appendChild(errorDiv);
-    } else hideLoading();
-  });
+      hideLoading();
+    });
 }
